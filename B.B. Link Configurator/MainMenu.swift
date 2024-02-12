e691c7c9adb307a112f0e7bdcddb6204588754f1
Georges Auberger <3819+georges@users.noreply.github.com>
2024-02-11 14:12:24 -1000

    OTA firmware flashing (#1)

--- B.B. Link Configurator/MainMenu.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MainMenu.swift
-//  Basic Chat
-//
-//  Created by Rob Riggs on 12/29/18.
-//  Copyright © 2018 Vanguard Logic LLC. All rights reserved.
-//
-
-import Foundation
--- B.B. Link Configurator/UUIDKey.swift
+++ B.B. Link Configurator/UUIDKey.swift
@@ -11,8 +11,15 @@ import CoreBluetooth
 let kBLEService_UUID = "00000001-ba2a-46c9-ae49-01b0961f68bb"
 let kBLE_Characteristic_uuid_Tx = "00000002-ba2a-46c9-ae49-01b0961f68bb"
 let kBLE_Characteristic_uuid_Rx = "00000003-ba2a-46c9-ae49-01b0961f68bb"
-let MaxCharacters = 20
 
 let BLEService_UUID = CBUUID(string: kBLEService_UUID)
 let BLE_Characteristic_uuid_Tx = CBUUID(string: kBLE_Characteristic_uuid_Tx)//(Property = Write without response)
 let BLE_Characteristic_uuid_Rx = CBUUID(string: kBLE_Characteristic_uuid_Rx)// (Property = Read/Notify)
+
+let kBLEServiceOTAUUID = "1A68D2B0-C2E4-453F-A2BB-B659D66CF442"
+let kBLECharacteristicOTAFlashUUID  = "1A68D2B1-C2E4-453F-A2BB-B659D66CF442"
+let kBLECharacterisitcOTAIdentityUUID =   "1A68D2B2-C2E4-453F-A2BB-B659D66CF442"
+
+let BLEServiceOTAUUID = CBUUID(string: kBLEServiceOTAUUID)
+let BLECharacteristicOTAFlashUUID = CBUUID(string: kBLECharacteristicOTAFlashUUID) // Write
+let BLECharacterisitcOTAIdentityUUID = CBUUID(string: kBLECharacterisitcOTAIdentityUUID) // Read
